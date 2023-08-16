97d05d47768038fc36613cda61c15f0284288091
Kenneth V. Domingo <hello@kvd.studio>
2023-08-16 23:24:36 +0800

    Bootstrap

---
+++ src/lib/CounterButton.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { writable } from "svelte/store";
+
+  const count = writable(0);
+
+  function handleClick() {
+    $count++;
+  }
+
+  $: doubled = $count * 2;
+</script>
+
+<div class="flex flex-col items-center gap-2">
+  <p>
+    Count: {$count}
+  </p>
+  <p>
+    Doubled: {doubled}
+  </p>
+  <button
+    class="rounded-xl bg-indigo-900 px-12 py-2 transition-colors hover:bg-indigo-700 active:bg-indigo-800"
+    on:click={handleClick}
+  >
+    <slot />
+  </button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+  import CounterButton from "$lib/CounterButton.svelte";
+</script>
+
+<div
+  class="flex h-screen w-full flex-col items-center justify-center gap-4 bg-gradient-to-b from-indigo-900 to-indigo-950"
+>
+  <h1>Welcome to SvelteKit!</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <CounterButton>count!</CounterButton>
+</div>
+
+<style lang="postcss">
+  :global(body) {
+    background-color: theme(colors.sky.950);
+    color: white;
+    font-size: 14pt;
+  }
+  :global(h1) {
+    font-size: theme(fontSize.4xl);
+  }
+  :global(h2) {
+    font-size: theme(fontSize.3xl);
+  }
+  :global(h3) {
+    font-size: theme(fontSize.2xl);
+  }
+  :global(h4) {
+    font-size: theme(fontSize.xl);
+  }
+  :global(h5) {
+    font-size: theme(fontSize.lg);
+  }
+  :global(a) {
+    color: theme(colors.sky.300);
+  }
+</style>
